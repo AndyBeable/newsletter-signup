affb2e0b93a2a12a65f6e5739b7ee7ded1402a92
AndyBeable <andy.beable@gmail.com>
2023-11-18 18:09:44 +0000

    adds tailwind, sets up boilerplate and first styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        "3xl": "0px 4px 4px 0px rgba(0,0,0,0.25)",
+      },
+      backgroundImage: {
+        sign_up_mb: "url('/assets/images/illustration-sign-up-mobile.svg')",
+      },
+    },
+    colors: {
+      "slate-grey": "#27293F",
+      white: "#ffffff",
+      "dark-navy": "#242742",
+    },
+    fontFamily: {
+      sans: ['"Roboto"'],
+    },
   },
   plugins: [],
 };
